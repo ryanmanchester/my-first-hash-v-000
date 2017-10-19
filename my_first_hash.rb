3aee69fcad34a662f7b24a86d78b833aea915352
Ryan Manchester <ryanm17@gmail.com>
2017-10-19 04:07:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,10 +21,7 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+   shipping_manifest["pearl necklace"] = 1
+   puts shipping_manifest
 
-  #your code here
-  #remember to return the shipping_manifest hash
-  shipping_manifest["pearl necklace"] = 1
-end
-shipping_manifest
 end
